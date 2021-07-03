c10c06787655d36fc6780f0eef8ad2d192811a2e
ngcdan <Linuss1908@gmail.com>
2021-07-03 20:07:12 +0700

    update code

---
+++ design.pattern/src/main/java/training/java/design/pattern/decorator/SandwichDecorator.java
@@ -0,0 +1,14 @@
+package training.java.design.pattern.decorator;
+
+public abstract class SandwichDecorator implements Sandwich {
+  protected Sandwich customSandwich;
+  
+  public SandwichDecorator(Sandwich customSandwich) {
+    this.customSandwich = customSandwich;
+  }
+  
+  public String make() {
+    return this.customSandwich.make();
+  }
+  
+}
--- design.pattern/src/test/java/training/java/design/pattern/composite/CompositeUnitTest.java
+++ design.pattern/src/test/java/training/java/design/pattern/composite/CompositeUnitTest.java
@@ -18,5 +18,4 @@ public class CompositeUnitTest {
     
     directory.showEmployeeDetails();
   }
-  
 }
