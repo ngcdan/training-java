b7b0e2da4b246f97438b9df51cebc8e91b0d9e03
ngcdan <Linuss1908@gmail.com>
2021-08-25 22:32:09 +0700

    update code

--- object-orented/src/main/java/training/java/object/orented/design/Employee.java
+++ object-orented/src/main/java/training/java/object/orented/design/Employee.java
@@ -2,30 +2,61 @@ package training.java.object.orented.design;
 
 import java.util.Currency;
 
-public class Employee {
-}
-
 interface TimeSheet{}
 interface Invoice{};
 class Money1 {
   public Money1(double val, Currency currency) {}
 }
 
-class Worker {
+interface Payable {
+  void pay();
+}
+
+abstract class Worker implements Payable {
   private String name;
   public Worker(String name) {
     this.name = name;
   }
   
-  public void pay() {/*...*/}
+  public void pay() {
+    Money1 due = getAmountDue();
+  }
+  
+  abstract protected Money1 getAmountDue();
+}
+
+public class Employee extends Worker {
+  private TimeSheet[] unpaidTimesheets;
+  
+  public Employee(String name) {super(name);}
+  
+  @Override
+  protected Money1 getAmountDue() {
+    //...
+    return new Money1(12.34, Currency.getInstance("USD"));
+  }
+  
+  public void attachTimesheet(TimeSheet i) {}
+}
+
+class Contractor extends Worker {
+ public Contractor(String name) {super(name);}
+  
+  @Override
+  protected Money1 getAmountDue() {
+    return null;
+  }
+  
+  public Invoice[] invoiceDue;
+  public void attachInvoice(Invoice i) {}
 }
 
 class AccountsPayable {
-  Worker[] workers;
+  Payable[] creators;
   
   public void payEveryBody() {
-    for(Worker worker: workers) {
-      worker.pay();
+    for(Payable payable: creators) {
+      payable.pay();
     }
   }
 }
