5bb9520129de2864f31c18730f69fe09ab87c2bc
ngcdan <Linuss1908@gmail.com>
2021-08-25 22:02:38 +0700

    update code

---
+++ object-orented/src/main/java/training/java/object/orented/design/Money.java
@@ -0,0 +1,21 @@
+package training.java.object.orented.design;
+
+public class Money {
+  private double value;
+  private Currency currency;
+  
+  public Money(double value, Currency currency) {
+    this.value = value;
+    this.currency = currency;
+  }
+  
+  public double normalized() {
+    return currency == Currency.USD
+      ? value
+      : value * currency.conversionRateTo(Currency.USD);
+  }
+  
+  public boolean isGreaterThan(Money op) {
+    return (normalized() > op.normalized());
+  }
+}
---
+++ object-orented/src/test/java/training/java/object/orented/DesignUnitTest.java
@@ -0,0 +1,20 @@
+package training.java.object.orented;
+
+import org.junit.jupiter.api.Test;
+import training.java.object.orented.design.Currency;
+import training.java.object.orented.design.Money;
+
+public class DesignUnitTest {
+  public static void dispenseFunds(Money money) {/*...*/};
+  
+  @Test
+  public void MoneyUnitTest() {
+    Money balance = new Money(1.0, Currency.EURO);
+    Money request = new Money(1.0, Currency.USD);
+    
+    if(balance.isGreaterThan(request)) {
+      dispenseFunds(request);
+    }
+  }
+}
+
