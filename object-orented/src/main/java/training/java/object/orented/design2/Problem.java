194bf00533d5d4112c28691999531ed64c52090b
ngcdan <Linuss1908@gmail.com>
2021-08-29 06:07:26 +0700

    update code

--- object-orented/src/main/java/training/java/object/orented/design2/Problem.java
+++ object-orented/src/main/java/training/java/object/orented/design2/Problem.java
@@ -1,23 +1,22 @@
 package training.java.object.orented.design2;
 
-public class Problem {
-}
+public class Problem {}
 
 class Worker {
   public void pay() { /* .. */}
 }
 
-class Employee extends training.java.object.orented.design2.solution.Worker {}
-class Contractor extends training.java.object.orented.design2.solution.Worker {}
+class Employee extends Worker {}
+class Contractor extends Worker {}
 
 //Problem: Interface Segregation Principal
-class Volunteer extends training.java.object.orented.design2.solution.Worker {
+class Volunteer extends Worker {
   public void pay() {
     throw new UnsupportedOperationException();
   }
 }
 
-class Vendor extends training.java.object.orented.design2.solution.Worker {
+class Vendor extends Worker {
   public void pay() {
     // Worker's implementation doesn't work here!
   }
