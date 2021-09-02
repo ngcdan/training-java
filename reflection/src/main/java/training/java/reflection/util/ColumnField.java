150b0427e6a02fcf860316839dcb249cc7da747c
ngcdan <Linuss1908@gmail.com>
2021-09-02 16:36:31 +0700

    add module algorithm

--- reflection/src/main/java/training/java/reflection/util/ColumnField.java
+++ reflection/src/main/java/training/java/reflection/util/ColumnField.java
@@ -7,20 +7,20 @@ import java.lang.reflect.Field;
 public class ColumnField {
   private Field field;
   private Column column;
-
+  
   public ColumnField(Field field) {
     this.field = field;
     this.column = this.field.getAnnotation(Column.class);
   }
-
+  
   public String getName() {
     return this.column.name();
   }
-
+  
   public Class<?> getType() {
     return field.getType();
   }
-
+  
   public Field getField() {
     return field;
   }
--- reflection/src/main/java/training/java/reflection/util/PrimaryKeyField.java
+++ reflection/src/main/java/training/java/reflection/util/PrimaryKeyField.java
@@ -7,20 +7,20 @@ import java.lang.reflect.Field;
 public class PrimaryKeyField {
   private Field field;
   private PrimaryKey primaryKey;
-
+  
   public PrimaryKeyField(Field field) {
     this.field = field;
     this.primaryKey = this.field.getAnnotation(PrimaryKey.class);
   }
-
+  
   public Class<?> getType() {
     return field.getType();
   }
-
+  
   public String getName() {
     return primaryKey.name();
   }
-
+  
   public Field getField() {
     return this.field;
   }
