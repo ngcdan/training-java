cbd04c7d005abf63bd9ef7b6ae9f089b05adb200
ngcdan <Linuss1908@gmail.com>
2021-09-02 22:52:52 +0700

    add frondend dir

--- app/src/main/java/training/java/app/App.java
+++
@@ -1,8 +0,0 @@
-package training.java.app;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("App running...!");
-        System.out.println(MessageUtils.getMessage());
-    }
-}
--- app/src/main/java/training/java/app/MessageUtils.java
+++
@@ -1,7 +0,0 @@
-package training.java.app;
-
-class MessageUtils {
-    public static String getMessage() {
-        return "Hello      World!";
-    }
-}
