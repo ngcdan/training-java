a28922861da533c3ec0fa8946ad435c2b4a5d2ee
ngcdan <Linuss1908@gmail.com>
2021-09-03 09:06:36 +0700

    play with multi thread

---
+++ module/concurrency/src/test/java/FirstRunnable.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+public class FirstRunnable {
+  
+  @Test
+  public void test() {
+    Runnable runnable = () -> {
+      System.out.println("I am running in " + Thread.currentThread().getName());
+    };
+    Thread t = new Thread(runnable);
+    t.setName("My Thread");
+    t.start();
+    //    t.run(); //NO using
+  }
+  
+}
