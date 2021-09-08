f44c2613ac5a856e1882d0e5ce0e5570390c391a
ngcdan <Linuss1908@gmail.com>
2021-09-08 20:28:00 +0700

    play with lambda

---
+++ module/streams/src/test/java/training/java/streams/LambdaExampleUnitTest.java
@@ -0,0 +1,35 @@
+package training.java.streams;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaExampleUnitTest {
+  
+  @Test
+  public void sortProductExampleUnitTest() {
+    List<Product> products = ExampleData.getProducts();
+    
+    products.sort(new Comparator<Product>() {
+      @Override
+      public int compare(Product p1, Product p2) {
+        return p1.getPrice().compareTo(p2.getPrice());
+      }
+    });
+    
+    for(Product product: products) {
+      System.out.println(product);
+    }
+  }
+  
+  @Test
+  public void sortProductExampleUsingLambdaUnitTest() {
+    List<Product> products = ExampleData.getProducts();
+    products.sort((p1, p2) -> p1.getPrice().compareTo(p2.getPrice()));
+    
+    for(Product product: products) {
+      System.out.println(product);
+    }
+  }
+}
