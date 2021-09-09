1258967c8419c0d091c2f86edb4274458ecef3c3
ngcdan <Linuss1908@gmail.com>
2021-09-09 22:43:20 +0700

    update code

---
+++ module/streams/src/test/java/training/java/streams/FuncInterfaceUnitTest.java
@@ -0,0 +1,107 @@
+package training.java.streams;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class FuncInterfaceUnitTest {
+  // Go through a list of products, and return the first product for which the predicate returns true.
+  static Optional<Product> findProduct(List<Product> products, Predicate<Product> predicate) {
+    for (Product product : products) {
+      if (predicate.test(product)) {
+        return Optional.of(product);
+      }
+    }
+    return Optional.empty();
+  }
+  
+  @Test
+  public void findProductsUnitTest() {
+    List<Product> products = ExampleData.getProducts();
+    
+    String name = "Spaghetti";
+    
+    // A combination of functional interfaces implemented by lambda expressions and method references is used here:
+    // - findProduct() takes a Predicate<Product> to find the product with the specified name
+    // - map() takes a Function<Product, BigDecimal> to get the price of the product
+    // - ifPresentOrElse() takes a Consumer<Product> and a Runnable and calls one of them, depending on whether
+    //      the Optional contains a value or is empty
+    
+    findProduct(products, product -> product.getName().equals(name))
+      .map(Product::getPrice)
+      .ifPresentOrElse(
+        price -> System.out.printf("The price of %s is $ %.2f%n", name, price),
+        () -> System.out.printf("%s is not available%n", name));
+  }
+  
+  @Test
+  public void example2UnitTest() {
+    List<Product> products = ExampleData.getProducts();
+    
+    Map<Category, List<Product>> productsByCategory = new HashMap<>();
+    
+    for (Product product : products) {
+      Category category = product.getCategory();
+      
+      // Check if the map already has a List for this category; if not, add one.
+      if (!productsByCategory.containsKey(category)) {
+        productsByCategory.put(category, new ArrayList<>());
+      }
+      
+      productsByCategory.get(category).add(product);
+    }
+    
+    for (Product product : products) {
+      Category category = product.getCategory();
+      
+      // computeIfAbsent() gets the existing List for the category, or calls the given
+      // Function<Category, List<Product>> to create the List if there is no entry in the Map for the category.
+      productsByCategory.computeIfAbsent(category, c -> new ArrayList<>()).add(product);
+    }
+    
+    // Map.forEach() takes a BiConsumer (a Consumer which takes two parameters); the key and value of each entry.
+    productsByCategory.forEach((category, ps) -> {
+      System.out.println("Category: " + category);
+      ps.forEach(product -> System.out.println("- " + product.getName()));
+    });
+  }
+  
+  @Test
+  public void example3UnitTest() {
+    List<Product> products = ExampleData.getProducts();
+    
+    String name = "Spaghetti";
+    
+    Function<Product, BigDecimal> productToPrice = Product::getPrice;
+    Function<BigDecimal, String> priceToMessage = price -> String.format("The price of %s is $ %.2f%n", name, price);
+    
+    Function<Product, String> productToMessage = productToPrice.andThen(priceToMessage);
+    //Function<Product, String> productToMessage = priceToMessage.compose(productToPrice);
+    
+    findProduct(products, product -> product.getName().equals(name))
+      .map(productToMessage)
+      .ifPresentOrElse(
+        System.out::println,
+        () -> System.out.printf("%s is not available%n", name));
+  }
+  
+  @Test
+  public void example4UnitTest() {
+    List<Product> products = ExampleData.getProducts();
+  
+    BigDecimal priceLimit = new BigDecimal("2.00");
+  
+    // Two simple predicates that can be combined using the functional composition methods in interface Predicate.
+    Predicate<Product> isFood = product -> product.getCategory() == Category.FOOD;
+    Predicate<Product> isCheap = product -> product.getPrice().compareTo(priceLimit) < 0;
+  
+    findProduct(products, isFood.and(isCheap)) // Combining the predicates with and(...)
+      .map(product -> String.format("%s for $ %.2f", product.getName(), product.getPrice()))
+      .ifPresentOrElse(
+        System.out::println,
+        () -> System.out.println("There are no cheap food products"));
+  }
+}
