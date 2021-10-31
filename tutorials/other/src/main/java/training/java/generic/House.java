68b682d402e3a8b0057cb5b928d03d8934bb84f1
ngcdan <Linuss1908@gmail.com>
2021-10-31 20:57:42 +0700

    update module generic

---
+++ tutorials/other/src/main/java/training/java/generic/House.java
@@ -0,0 +1,13 @@
+package training.java.generic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class House extends Building {
+  public static final Logger logger = LoggerFactory.getLogger(House.class);
+  
+  @Override
+  public void paint() {
+    logger.info("Painting house!");
+  }
+}
