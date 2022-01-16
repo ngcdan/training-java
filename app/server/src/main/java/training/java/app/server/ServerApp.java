16263421807f63962c09d02fff73432b22e0faa5
ngcdan <linuss1908@gmail.com>
2022-01-16 13:20:29 +0700

    fix bugs

--- app/server/src/main/java/training/java/app/server/ServerApp.java
+++ app/server/src/main/java/training/java/app/server/ServerApp.java
@@ -3,14 +3,12 @@ package training.java.app.server;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import training.java.core.settings.ModuleCoreSettingConfig;
 import training.java.core.settings.location.data.LocationData;
@@ -19,9 +17,52 @@ import training.java.core.settings.location.data.LocationData;
 @EnableConfigurationProperties
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 //@Import(ModuleCoreSettingConfig.class)
-public class ServerApp implements CommandLineRunner {
+public class ServerApp {
   static Logger logger = LoggerFactory.getLogger(ServerApp.class);
 
+  static ConfigurableApplicationContext context;
+
+  static public ApplicationContext run(String[] args, long wait) throws Exception {
+    logger.info("Launch ServerApp with args: {}", StringUtils.join(args, " "));
+    Class<?>[] sources = {ModuleCoreSettingConfig.class, ServerApp.class};
+    context = SpringApplication.run(sources, args);
+    isRunning(wait);
+
+    logger.info("Import data location.........");
+    LocationData data = new LocationData(context);
+    data.initialize();
+    return context;
+  }
+
+  static public boolean isRunning(long waitTime) {
+    boolean running = false;
+    if(waitTime <= 0) waitTime = 1;
+    try {
+      while(!running && waitTime > 0) {
+        if(context != null) running = context.isRunning();
+        waitTime -= 100;
+        if(!running && waitTime > 0) Thread.sleep(100);
+      }
+    } catch(Exception ex) {
+      ex.printStackTrace();
+    }
+    return running;
+  }
+
+  static public void exit() {
+    if(context != null) {
+      SpringApplication.exit(context);
+      context = null;
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    run(args, 30000);
+    Thread.currentThread().join();
+  }
+
+
+  /*
   @Autowired
   private ApplicationContext context;
 
@@ -41,4 +82,5 @@ public class ServerApp implements CommandLineRunner {
     LocationData data = new LocationData(context);
     data.initialize();
   }
+   */
 }
--- core/settings/src/test/java/training/java/core/settings/LocationUnitTest.java
+++ core/settings/src/test/java/training/java/core/settings/LocationUnitTest.java
@@ -1,5 +1,6 @@
 package training.java.core.settings;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import training.java.core.settings.location.CountryGroupLogic;
 import training.java.core.settings.location.data.LocationData;
+import training.java.core.settings.location.entity.CountryGroup;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,6 +32,8 @@ public class LocationUnitTest {
   public void dataParserUnitTest() throws Exception {
     LocationData data = context.getAutowireCapableBeanFactory().createBean(LocationData.class);
     data.initialize();
+    final List<CountryGroup> all = countryGroupLogic.findAll();
+    Assertions.assertTrue(all.size() > 1);
   }
 
 
