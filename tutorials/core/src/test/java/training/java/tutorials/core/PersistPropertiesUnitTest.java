c5ae35ddf6b1607e0e9787f9fdb303767db456ad
ngcdan <linuss1908@gmail.com>
2022-03-29 16:25:58 +0700

    merge code

--- tutorials/core/src/test/java/training/java/tutorials/core/PersistPropertiesUnitTest.java
+++ tutorials/core/src/test/java/training/java/tutorials/core/PersistPropertiesUnitTest.java
@@ -1,12 +1,14 @@
 package training.java.tutorials.core;
 
+import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
-import org.junit.jupiter.api.Test;
 
 public class PersistPropertiesUnitTest {
 
@@ -32,5 +34,24 @@ public class PersistPropertiesUnitTest {
     } catch(IOException e) {
       e.printStackTrace();
     }
+  }
+
+  @Test
+  public void loadProps() {
+    Properties props = new Properties();
+    try (Reader reader = Files.newBufferedReader(Paths.get("src/myapp.properties"))) {
+      props.load(reader);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    String val1 = props.getProperty("val1");
+    String val2 = props.getProperty("val2");
+    String val3 = props.getProperty("val3");
+    String val4 = props.getProperty("val4");
+    boolean x = val1.equals(val2);
+    boolean y = val3.equals(val4);
+    System.out.println(x);
+    System.out.println(y);
   }
 }
